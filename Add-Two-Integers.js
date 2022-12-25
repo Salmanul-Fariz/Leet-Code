2d5d44d66c6638ea0a4f579fa29ab4f1b7892ed5
Salmanul Fariz N <salmanulfariz997@gmail.com>
2022-12-25 12:30:55 +0530

    how-many-numbers-are-smaller-than-the-current-number

--- Add-Two-Integers.js
+++ Add-Two-Integers.js
@@ -1,5 +1,7 @@
 /* 
+
 Q:: Given two integers num1 and num2, return the sum of the two integers.
+
 */
 
 const sum = function (num1, num2) {
@@ -9,7 +11,9 @@ const sum = function (num1, num2) {
 console.log(sum(12, 5));
 
 /*
+
 Input: num1 = 12, num2 = 5
 Output: 17
 Explanation: num1 is 12, num2 is 5, and their sum is 12 + 5 = 17, so 17 is returned.
+
 */
--- Two-Sum.js
+++ Two-Sum.js
@@ -1,7 +1,11 @@
-/* Q :: Given an array of integers nums and an integer target,
+/* 
+
+Q :: Given an array of integers nums and an integer target,
 return indices of the two numbers such that they add up to target. 
 You may assume that each input would have exactly one solution,
-and you may not use the same element twice. You can return the answer in any order.*/
+and you may not use the same element twice. You can return the answer in any order.
+
+*/
 
 const twoSum = function (nums, target) {
   let i = 0;
@@ -27,7 +31,9 @@ const twoSum = function (nums, target) {
 console.log(twoSum([2, 7, 11, 15], 9));
 
 /*
+
 Input: nums = [2,7,11,15], target = 9
 Output: [0,1]
 Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
- */
+ 
+*/
